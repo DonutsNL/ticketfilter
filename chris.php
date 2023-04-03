a13e0e91e3dd04d154271e256e5874368e4938cc
DonutsNL <97617761+DonutsNL@users.noreply.github.com>
2023-04-03 20:49:18 +0200

    voorbeeld

---
+++ chris.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Hier zijn onze uitgangspunten
+ * 
+ * We willen voldoen aan SOLID;
+ * We passen type safety toe waar mogelijk;
+ * We passen SONAR lint toe waar mogelijk
+ *  We negeren de regellengte
+ * 
+ * We ruimen alle code sniffs direct op;
+ * 
+ * 
+ */
