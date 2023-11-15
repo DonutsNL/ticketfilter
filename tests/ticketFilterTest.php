c757e4e11f1c15394bd7cfbb727c14cd2afc1daa
DonutsNL <chris.gralike@flevo-scouts.nl>
2023-11-15 20:28:24 +0000

    Added some basic tests.

--- tests/ticketFilterTest.php
+++ tests/ticketFilterTest.php
@@ -14,6 +14,36 @@ class ticketFilterTest extends Testcase
     public function testTicketHandler()
     {
         $tf = new TicketHandler();
+
+        
+        print("\nEvaluate initHandler exists in TicketHandler::class : ");
+        if(!$this->assertTrue(
+            method_exists(TicketHandler::class, 'initHandler'), 
+            'Class TicketHandler::class does not have method initHandler'
+        )){ print("Ok!\n"); }
+
+        print("Evaluate getId exists in TicketHandler::class : ");
+        if(!$this->assertTrue(
+            method_exists(TicketHandler::class, 'getId'), 
+            'Class TicketHandler::class does not have method getId'
+        )){ print("Ok!\n"); }
+
+        print("Evaluate TicketHandler->getId returns interger : ");
+        if(!$this->assertTrue(is_int($tf->getId(1)), 
+            'Class TicketHandler::getId(1) did not return integer')){
+            print("Ok!\n");
+        };
+        
+        print("Evaluate addSolvedMessage exists in TicketHandler::class : ");
+        if(!$this->assertTrue(
+            method_exists(TicketHandler::class, 'addSolvedMessage'), 
+            'Class TicketHandler::class does not have method addSolvedMessage'
+        )){ print("Ok!\n"); }
+        
+        $this->assertTrue(
+            method_exists(TicketHandler::class, 'addSolvedMessage'), 
+            'Class TicketHandler::class does not have method addSolvedMessage'
+        );
     }
 
     public function testWithoutDelay()
